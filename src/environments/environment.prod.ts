577300ebcdcaadf904ac9967d38fa1992ee1815a
unknown <d.depta90@gmail.com>
2020-05-17 23:52:02 +0200

    Firebase conig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    // your config here
+    apiKey: 'AIzaSyAT4m8O3Ul07Ih_Os4A-6sV7lC4ZmxBICo',
+    authDomain: 'skplanes-71fd7.firebaseapp.com',
+    databaseURL: 'https://skplanes-71fd7.firebaseio.com',
+    projectId: 'skplanes-71fd7',
+    storageBucket: 'skplanes-71fd7.appspot.com',
+    messagingSenderId: '655926545829',
+    appId: '1:655926545829:web:526f291e2365378357ac4c'
   }
 };
